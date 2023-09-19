47e4e523533fc1ccd97f7b844777acc3a40c7243
ktw09876 <dhqhfjem@gmail.com>
2023-09-19 21:04:48 +0900

    기존 데이터프레임에 새로운 데이터프레임을 추가 ing...

--- COVID_19/data_load_test.py
+++ COVID_19/data_load_test.py
@@ -1,23 +1,14 @@
-from pyspark.sql import SparkSession
-from pyspark.sql import Row
-
 import os
 import requests
+import pandas as pd
+
 from datetime import datetime
 from bs4 import BeautifulSoup
 from itertools import product
 
 
-#SparkSession 생성
-spark = SparkSession.builder.appName("Read_CSV").getOrCreate()
-
-# 내가 접속한 브라우저 정보를 알려줌, 로봇이 아닌 사람이 접근했다고 알리는 기능
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-}
-
-
-def url_mapping(url_list):
+#넘겨 받은 download_url_list를 각 월에 맞는 길이로 나눠서 데이터프레임을 생성 후 기존 데이터프레임에 추가하는 함수 ing...
+def url_mapping(url_list_in):
     #불러올 .csv 경로
     int_months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11']
     csv_file = ''
@@ -32,8 +23,8 @@ def url_mapping(url_list):
             if file_name.endswith('.csv'):
                 csv_file = file_name
         
-        df = spark.read.csv(
-                    f'{covid_paths}{csv_file}'
+        df = pd.read_csv(
+                      f'{covid_paths}{csv_file}'
                     , header = True
                     , inferSchema = True
                 )
@@ -41,22 +32,45 @@ def url_mapping(url_list):
     #url_list를 월 날짜 개수 별로 나눔
     for days in days_in_each_month:
         end_idx = start_idx + days
-        result.append(url_list[start_idx:end_idx])
+        result.append(url_list_in[start_idx:end_idx])
         start_idx = end_idx
     
         # 새로운 행을 생성
-        values = ['report_down'] + result # 'report_down'은 첫번째 컬럼, 나머지는 result
-        new_row = Row(*values)
+        df = df.append(new_row, ignore_index=True)
     
-    new_df = df.union(spark.createDataFrame([new_row]))
+    new_df = df.union(pd.DataFrame([new_row]))
     new_df.tail(5)
 
+#코로나 역학 보고서의 download url가 있는 페이지의 url을 생성하는 함수 매개변수 없음
+def get_downlaod_url_list():
 
+    #report_url 생성
+    years = ['2020', '2021', '2022', '2023']
+    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november']
+    days = [str(i) for i in range(1, 32)]
 
-def download_url(url):
-    response = requests.get(url)
+    # 모든 조합 생성
+    combinations = product(years, months, days)
+
+    result_list = []
+    for year, month, day in combinations:
+        url1 = f'https://www.who.int/publications/m/item/weekly-update-on-covid-19---{day}-{month}-{year}'
+        # url2 = f'https://www.who.int/publications/m/item/weekly-epidemiological-update---{day}-{month}-{year}'
+
+        downlaod_url_list = result_list.append(get_download_url(url1))
+    
+    return downlaod_url_list
+
+#url을 넘겨 받아서 크롤링으로 코로나 역학 보고서의 download url을 얻어오는 함수
+def get_download_url(url):
+
+    # 내가 접속한 브라우저 정보를 알려줌, 로봇이 아닌 사람이 접근했다고 알리는 기능
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    }
+    
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:  # 상태 코드가 200일 때만
-        # print(f"URL: {url}, Status Code: {status_code}")
 
         # #BeautifulSoup()로 웹페이지 분석
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -74,56 +88,8 @@ def download_url(url):
     else:
         return ''
 
-def get_download_url():
-    #report_url 생성
-    years = ['2020', '2021', '2022', '2023']
-    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november']
-    days = [str(i) for i in range(1, 32)]
-
-    # 모든 조합 생성
-    combinations = product(years, months, days)
-    date_list = []
-    url_list = []
 #https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports
-    for year, month, day in combinations:
-        url1 = f'https://www.who.int/publications/m/item/weekly-update-on-covid-19---{day}-{month}-{year}'
-        # url2 = f'https://www.who.int/publications/m/item/weekly-epidemiological-update---{day}-{month}-{year}'
-
-        url_list.append(download_url(url1))
-
-        # url_status(url2)
-
-        # URL에서 날짜 정보 추출
-        # date_str = url1.split('---')[-1]
-
-        #date_str의 첫번째 스펠링을 대문자로
-        # date_str = date_str.capitalize()
-        # try:
-        #     date = datetime.strptime(date_str, '%d-%B-%Y').strftime('%Y/%m/%d')
-        #     date_list.append(date)
-        # except:
-        #     print('error 날짜 범위르 벗어났습니다')
-
-        return url_list
-    
+url_list = []
 if __name__ == "__main__":
-
-    download_url_list = get_download_url()
-    url_mapping(download_url_list)
-
-    #데이터프레임의 컬럼 날짜와 동일한 url의 날짜의 download_url을 추가 ing...
-    # for col in df.columns:
-    #     if col == date:
-            
-
-    
-
-    # combinations = product(years_in, months_is, days_is)
-
-    # for year, month, day in combinations:
-    #     url1 = f'https://www.who.int/publications/m/item/weekly-update-on-covid-19---{day}-{month}-{year}'
-
-
-
-
-spark.stop()
+    url_list = get_downlaod_url_list()
+    url_mapping(url_list)

c222631324c9d390d059a36f2f4c4939efdae25d
ktw09876 <dhqhfjem@gmail.com>
2023-09-21 21:11:45 +0900

    selenium을 이용한 크롤링 테스트 ing...

--- COVID_19/test3.py
+++ COVID_19/test3.py
@@ -1,60 +1,50 @@
-import asyncio
-import aiohttp
-from bs4 import BeautifulSoup
-from itertools import product
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-# 내가 접속한 브라우저 정보를 알려줌, 로봇이 아닌 사람이 접근했다고 알리는 기능
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-}
+# #크롬 드라이버 자동 업데이트
+# from webdriver_manager.chrome import ChromeDriverManager
 
-async def url_status(session, url):
-    try:
-        async with session.get(url, headers=headers) as response:
-            status_code = response.status
-            if status_code == 200:  # 상태 코드가 200일 때만 출력
-                # print(f"URL: {url}, Status Code: {status_code}")
+#옵션 설정
+options = webdriver.ChromeOptions()
 
-                # #BeautifulSoup()로 웹페이지 분석
-                soup = BeautifulSoup(response.text, 'html.parser')
+# headless 옵션 설정
+options.add_argument('headless')
+options.add_argument("no-sandbox")
 
-                # # #COVID-19 역학 보고서 버튼 찾기
-                download_btn = soup.select_one('#PageContent_C001_Col00 > article > section > div > div.dynamic-content__figure-container > div > a')
-                print(download_btn)
+# 브라우저 윈도우 사이즈
+options.add_argument('window-size=1920x1080')
 
-                # #COVID-19 역학 보고서 버튼에서 보고서 다운 url 추출
-                onclick_list = download_btn['onclick'].split("'")
-                for onclick in onclick_list:
-                    if onclick.startswith('https://'):
-                        download_url = onclick
-                        break
+# 사람처럼 보이게 하는 옵션들
+options.add_argument("disable-gpu")   # 가속 사용 x
+options.add_argument("lang=ko_KR")    # 가짜 플러그인 탑재
+options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')  # user-agent 이름 설정
 
-                print(download_url)
+#브라우저 꺼짐 방지
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
-    except Exception as e:
-        print(f"실패! URL: {url}, Exception: {e}, Status Code: {status_code}")
+#불필요한 에러메시지 없애기
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-async def download_url():
-    years = ['2020', '2021', '2022', '2023']
-    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
-    days = [str(i) for i in range(1, 32)]
+#웹 브라우저 띄우기
+browser = webdriver.Chrome(options = chrome_options)
 
-    # 모든 조합 생성
-    combinations = product(years, months, days)
+browser.maximize_window() #화면 최대화
+browser.get('https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports')
+browser.implicitly_wait(2) #로딩이 끝날때까지 10를 기다림
 
-    async with aiohttp.ClientSession() as session:
-        tasks = []
+#쇼핑 매뉴 클릭
+browser.find_element(By.CSS_SELECTOR, '#PageContent_C006_Col01 > div:nth-child(4) > a').click()
+# time.sleep(5) # 쇼핑 배너를 클릭한 뒤, 아직 페이지가 뜨지도 않았는데 바로 다음 명령어가 실행될 수도 있으니까, 2초 정도 여유를 준다.
 
-        for year, month, day in combinations:
-            url1 = f'https://www.who.int/publications/m/item/weekly-update-on-covid-19---{day}-{month}-{year}'
-            url2 = f'https://www.who.int/publications/m/item/weekly-epidemiological-update---{day}-{month}-{year}'
-            task1 = url_status(session, url1)
-            tasks.append(task1)
-            task2 = url_status(session, url2)
-            tasks.append(task2)
-        
-        await asyncio.gather(*tasks)  # 비동기 작업을 병렬로 실행
+# #검색창 클릭 
+# search = browser.find_element(By.CSS_SELECTOR, 
+#     '#__next > div > div.pcHeader_header__tXOY4 > div > div > div._gnb_header_area_150KE > div > div._gnbLogo_gnb_logo_3eIAf > div > div._gnbSearch_gnb_search_3O1L2 > form > div._gnbSearch_inner_2Zksb > div > input'
+#                               ).click() #적용 안됨 원인 찾아보기
 
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(download_url())
+
+# # #검색어 입력
+# search.send_keys('아이폰 13')
+# search.send_keys(Keys.ENTER)

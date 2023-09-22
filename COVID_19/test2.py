48f466f3920c127a6ff5cee0aa5abcec09e77c3e
ktw09876 <dhqhfjem@gmail.com>
2023-09-22 11:58:43 +0900

    코로나 역학 보고서 크롤링 ing...

--- COVID_19/test2.py
+++ COVID_19/test2.py
@@ -1,31 +1,20 @@
-import findspark 
-findspark.init()
+import requests
+from bs4 import BeautifulSoup
 
-from pyspark.sql import SparkSession
-import pandas as pd
+url = "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports"
 
-# 스파크 세션 생성
-spark = SparkSession.builder.appName("pivot_test").getOrCreate()
+response = requests.get(url)
 
-# 주어진 데이터프레임 생성
-data = [
-    ("Afghanistan", 203167, 203265, 203395, 203497, 203574, 203681, 203829, 203942, 204094, 204287, 204392, 204417, 204510),
-    ("Albania", 332969, 332996, 332996, 333027, 333046, 333055, 333058, 333071, 333088, 333103, 333125, 333138, 333156),
-    ("Algeria", 270839, 270840, 270847, 270856, 270862, 270873, 270881, 270891, 270906, 270917, 270924, 270929, 270939),
-    # 다른 국가/지역 데이터도 추가
-]
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
 
-columns = ["Country_Region", "2022/11/01", "2022/11/02", "2022/11/03", "2022/11/04", "2022/11/05", "2022/11/06", "2022/11/07", "2022/11/08", "2022/11/09", "2022/11/10", "2022/11/11", "2022/11/12", "2022/11/13"]
+    # 날짜별 보고서 링크를 담고 있는 요소를 찾습니다.
+    report_links = soup.find_all("a", {"class": "link-container"})
 
-df = spark.createDataFrame(data, columns)
-df.show()
-
-# 데이터프레임을 Pandas로 변환
-pandas_df = df.toPandas()
-
-# "Country_Region" 열을 인덱스로 설정
-pandas_df.set_index("Country_Region", inplace=True)
-
-# 행과 열을 바꾸기 위해 transpose (T) 사용
-transposed_df = pandas_df.T
-print(transposed_df)
+    # 각 보고서의 링크와 제목을 출력합니다.
+    for link in report_links:
+        report_url = link.get("href")
+        report_title = link.text.strip()
+        print(report_title, ": ", report_url)
+else:
+    print("웹 페이지를 가져올 수 없습니다.")
--- COVID_19/test3.py
+++ COVID_19/test3.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
+import time
 
 # #크롬 드라이버 자동 업데이트
 # from webdriver_manager.chrome import ChromeDriverManager
@@ -11,40 +12,59 @@ options = webdriver.ChromeOptions()
 
 # headless 옵션 설정
 options.add_argument('headless')
-options.add_argument("no-sandbox")
-
-# 브라우저 윈도우 사이즈
-options.add_argument('window-size=1920x1080')
+options.add_argument('no-sandbox')
 
 # 사람처럼 보이게 하는 옵션들
-options.add_argument("disable-gpu")   # 가속 사용 x
-options.add_argument("lang=ko_KR")    # 가짜 플러그인 탑재
+options.add_argument('disable-gpu')   # 가속 사용 x
+options.add_argument('lang=ko_KR')    # 가짜 플러그인 탑재
 options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')  # user-agent 이름 설정
 
 #브라우저 꺼짐 방지
 chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+chrome_options.add_experimental_option('detach', True)
 
 #불필요한 에러메시지 없애기
-chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+url = 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports'
 
 #웹 브라우저 띄우기
 browser = webdriver.Chrome(options = chrome_options)
 
 browser.maximize_window() #화면 최대화
-browser.get('https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports')
-browser.implicitly_wait(2) #로딩이 끝날때까지 10를 기다림
+browser.get(url)
+browser.implicitly_wait(2) #로딩이 끝날때까지 2초를 기다림
 
-#쇼핑 매뉴 클릭
-browser.find_element(By.CSS_SELECTOR, '#PageContent_C006_Col01 > div:nth-child(4) > a').click()
-# time.sleep(5) # 쇼핑 배너를 클릭한 뒤, 아직 페이지가 뜨지도 않았는데 바로 다음 명령어가 실행될 수도 있으니까, 2초 정도 여유를 준다.
+def get_download_url(selector):
+    try:
 
-# #검색창 클릭 
-# search = browser.find_element(By.CSS_SELECTOR, 
-#     '#__next > div > div.pcHeader_header__tXOY4 > div > div > div._gnb_header_area_150KE > div > div._gnbLogo_gnb_logo_3eIAf > div > div._gnbSearch_gnb_search_3O1L2 > form > div._gnbSearch_inner_2Zksb > div > input'
-#                               ).click() #적용 안됨 원인 찾아보기
+        #COVID-19 역학 보고서 버튼 찾기
+        browser.find_element(By.CSS_SELECTOR, selector).click()
+    except:
+        print(f'에러!!! {selector}를 다시 확인해주세요')
+        time.sleep(2) # 아직 페이지가 뜨지도 않았는데 바로 다음 명령어가 실행될 수도 있으니까, 2초 정도 여유를 준다.
+
+    try:
+        #download_btn 찾기
+        download_btn = browser.find_element(By.CSS_SELECTOR, '#PageContent_C001_Col00 > article > section > div > div.dynamic-content__figure-container > div > a')
+
+        #download_btn에서 COVID_19 역학 보고서 다운 url 추출
+        onclick = download_btn.get_attribute('onclick')
+        download_url = onclick.split("'")[3]
+        print(download_url)
+    except:
+        print(f'에러! {selector}를 다시 확인해주세요')
+        time.sleep(2) 
+    
+    
+#PageContent_C006_Col01 > div:nth-child(6) > a:nth-child(1)
+#PageContent_C006_Col01 > div:nth-child(6) > a:nth-child(1)
+
+for i in range(6, 80):
+    for j in range(1, 11):
+        selector = f'#PageContent_C006_Col01 > div:nth-child({i}) > a:nth-child({j})'
+        get_download_url(selector)
 
 
-# # #검색어 입력
-# search.send_keys('아이폰 13')
-# search.send_keys(Keys.ENTER)
+
+
